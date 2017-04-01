351ff3bc22d078590eba6d3d2ba9f7a42ea18444
Sheldon Benard <sheldon.benard@mail.mcgill.ca>
2017-04-01 15:50:42 -0600

    regInfo page

--- meeton/js/script.js
+++ meeton/js/script.js
@@ -149,7 +149,7 @@
 
 	//Countdown Timer
 	if($('#countdown-timer').length){
-		$('#countdown-timer').countdown('2017/11/29', function(event) {
+		$('#countdown-timer').countdown('2017/11/09', function(event) {
 			var $this = $(this).html(event.strftime('' + '<div class="counter-column"><span class="count">%D</span><span class="colon">:</span><br>DAYS</div> ' + '<div class="counter-column"><span class="count">%H</span><span class="colon">:</span><br>HOURS</div>  ' + '<div class="counter-column"><span class="count">%M</span><span class="colon">:</span><br>MINUTES</div>  ' + '<div class="counter-column"><span class="count">%S</span><br>SECOND</div>'));
 		});
 	}
